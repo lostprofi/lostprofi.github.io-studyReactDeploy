8d899e7b55484dc5b324724505b5de40acdb5fb7
lostprofi <o-lazarchik@mail.ru>
2020-02-18 23:08:47 +0300

    Updates

---
+++ precache-manifest.7fe08ab330a605cf779acff7a083588f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599bab9600b0f661c18f0c133968681d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "692736836e0805539343",
+    "url": "/static/css/main.3ce95e95.chunk.css"
+  },
+  {
+    "revision": "7406b6081e38645823ed",
+    "url": "/static/js/2.42846b99.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.42846b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "692736836e0805539343",
+    "url": "/static/js/main.91807b86.chunk.js"
+  },
+  {
+    "revision": "422f001d134cd330355e",
+    "url": "/static/js/runtime-main.a7a32bf1.js"
+  },
+  {
+    "revision": "08a754d1d68f4671eab5e11ece77cd03",
+    "url": "/static/media/Montserrat-Regular.08a754d1.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "740b8e9adcb998d4f65b3b18251fb5e2",
+    "url": "/static/media/WorkSans-Regular.740b8e9a.ttf"
+  },
+  {
+    "revision": "6df572c60aa0ac7d7f47ace0745b8e20",
+    "url": "/static/media/logo.6df572c6.svg"
+  },
+  {
+    "revision": "74e6a8b0e0bc4bfe90f9a14080b7fdcd",
+    "url": "/static/media/search_form_background.74e6a8b0.jpg"
+  }
+]);
